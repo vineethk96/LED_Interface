d698cc40f931cb9b7fe4b0419b62c0b772d26b59
VineethK <vineethk@vt.edu>
2022-08-21 14:00:39 -0400

    created functions for users to use

--- LED_Interface/LED_Interface.cpp
+++ LED_Interface/LED_Interface.cpp
@@ -1,2 +1,59 @@
 #include "LED_Interface.h"
 
+LED_Interface::LED_Interface(uint8_t pin0, uint8_t pin1, uint8_t pin2, uint8_t pin3, uint8_t pin4){
+
+    led_array[0] = pin0;
+    led_array[1] = pin1;
+    led_array[2] = pin2;
+    led_array[3] = pin3;
+    led_array[4] = pin4;
+}
+
+LED_Interface::LED_Interface(uint8_t pin0, uint8_t pin1, uint8_t pin2, uint8_t pin3){
+    led_array[0] = pin0;
+    led_array[1] = pin1;
+    led_array[2] = pin2;
+    led_array[3] = pin3;
+}
+
+LED_Interface::LED_Interface(uint8_t pin0, uint8_t pin1, uint8_t pin2){
+    led_array[0] = pin0;
+    led_array[1] = pin1;
+    led_array[2] = pin2;
+}
+
+LED_Interface::LED_Interface(uint8_t pin0, uint8_t pin1){
+    led_array[0] = pin0;
+    led_array[1] = pin1;
+}
+
+LED_Interface::LED_Interface(uint8_t pin0){
+    led_array[0] = pin0;
+}
+
+void LED_Interface::To_Red(uint8_t led){
+
+}
+
+void LED_Interface::To_Blue(uint8_t led){
+
+}
+
+void LED_Interface::To_Green(uint8_t led){
+
+}
+
+void LED_Interface::LED_Off(uint8_t led){
+
+}
+
+void LED_Interface::LED_All_Off(){
+
+    for(uint8_t i = 0; i < arr_size; ++i){
+        LED_Off(led_array[i]);
+    }
+}
+
+void LED_Interface::Change_Brightness(uint8_t led, uint8_t percent_brightness){
+
+}
--- LED_Interface/LED_Interface.h
+++ LED_Interface/LED_Interface.h
@@ -2,9 +2,32 @@
 #define LED_INTERFACE_H
 
 // Include STM32 HAL Lib
+#include <stdint.h>
 
 class LED_Interface{
 
+    private:
+
+        static const uint8_t arr_size = 5;
+
+        // Holds the pin numbers for all 5 LEDs
+        uint8_t led_array[arr_size];
+
+    public:
+        // Overloaded Constructors
+        LED_Interface(uint8_t pin1, uint8_t pin2, uint8_t pin3, uint8_t pin4, uint8_t pin5);
+        LED_Interface(uint8_t pin1, uint8_t pin2, uint8_t pin3, uint8_t pin4);
+        LED_Interface(uint8_t pin1, uint8_t pin2, uint8_t pin3);
+        LED_Interface(uint8_t pin1, uint8_t pin2);
+        LED_Interface(uint8_t pin1);
+
+        // Change State of LED
+        void To_Red(uint8_t led);
+        void To_Blue(uint8_t led);
+        void To_Green(uint8_t led);
+        void LED_Off(uint8_t led);
+        void LED_All_Off();
+        void Change_Brightness(uint8_t led, uint8_t percent_brightness);
 
 
 };
